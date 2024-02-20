fdf32aec15a1f8d25998fb207425032f3007f9cd
rknabe <rusty.knabe@gmail.com>
2024-02-20 17:29:16 -0600

    cleanup

--- src/Due_QDEC.h
+++ src/Due_QDEC.h
@@ -4,10 +4,9 @@
 #include <Arduino.h>
 #include "YokeConfig.h"
 
-
 #ifdef _VARIANT_ARDUINO_DUE_X_
-const int quadX_A = 2;
-const int quadX_B = 13;
+const int quadX_A = encoderPin_XA;
+const int quadX_B = encoderPin_XB;
 const unsigned int mask_quadX_A = digitalPinToBitMask(quadX_A);
 const unsigned int mask_quadX_B = digitalPinToBitMask(quadX_B);
 
@@ -16,20 +15,19 @@ const int quadY_B = 4;
 const unsigned int mask_quadY_A = digitalPinToBitMask(quadY_A);
 const unsigned int mask_quadY_B = digitalPinToBitMask(quadY_B);
 
+class Due_QDEC
+{
+public:
+  Due_QDEC(void);
+  ~Due_QDEC(void);
+  Axis axis[2];
+  void setConfig();
+  void initVariables(void);
+  void updatePosition(int idx);
 
-class Due_QDEC {
-  public:
-    Due_QDEC(void);
-    ~Due_QDEC(void);
-    Axis axis[2];
-    void setConfig();
-    void initVariables(void);
-    void updatePosition(int idx);
-    
-    void activateCNT_TC0();  
-    void activateCNT_TC2();
-    void Reset_Encoder(int idx);
-
+  void activateCNT_TC0();
+  void activateCNT_TC2();
+  void Reset_Encoder(int idx);
 };
 
 #endif
